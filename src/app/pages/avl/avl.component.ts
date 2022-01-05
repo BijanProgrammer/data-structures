bdc53efef62a5226a6c60d3da6a1d6f697a19db6
BijanProgrammer <bijaneisapour@gmail.com>
2022-01-05 11:15:43 +0330

    [REMOVE] redundant code

--- src/app/pages/avl/avl.component.ts
+++ src/app/pages/avl/avl.component.ts
@@ -13,7 +13,7 @@ export class AvlComponent {
     public Layout = Layout;
 
     public randomTreeGenerator = new BigAvlTreeGenerator();
-    public methods: string[] = ['leftRotation', 'rightRotation', 'leftRightRotation', 'rightLeftRotation', 'add'];
+    public methods: string[] = ['leftRotation', 'rightRotation', 'leftRightRotation', 'rightLeftRotation'];
 
     private animationSteps!: OgmaAnimationStep[];
     private graphVisualizerComponent!: GraphVisualizerComponent;
@@ -46,9 +46,6 @@ export class AvlComponent {
             case 'rightLeftRotation':
                 this.rightLeftRotation(+value);
                 break;
-            // case 'add':
-            //     this.add(+value);
-            //     break;
         }
     }
 
@@ -498,36 +495,6 @@ export class AvlComponent {
         this.animationSteps.push({actions: nodeRotateLeftActions});
     }
 
-    // private add(x: number): void {
-    //     let node = this.graphVisualizerComponent.getNode(1);
-    //     const nodeId = this.graphVisualizerComponent.getNodes().size + 1;
-    //     const edgeId = this.graphVisualizerComponent.getEdges().size + 1;
-    //
-    //     while (true) {
-    //         const currentIndex = node.getId();
-    //         const currentValue = +node.getAttribute('text');
-    //
-    //         this.markAsVisited(currentIndex);
-    //         const {left, right} = this.right(currentIndex);
-    //
-    //         if (x <= currentValue) {
-    //             if (!left) {
-    //                 this.addNode(node, 0, nodeId, edgeId, x);
-    //                 return;
-    //             }
-    //
-    //             node = this.graphVisualizerComponent.getNode(left);
-    //         } else {
-    //             if (!right) {
-    //                 this.addNode(node, 1, nodeId, edgeId, x);
-    //                 return;
-    //             }
-    //
-    //             node = this.graphVisualizerComponent.getNode(right);
-    //         }
-    //     }
-    // }
-
     private nodeOnTheRightOf(node: Node): Node | undefined {
         const [first, second]: Edge[] = node.getAdjacentEdges({direction: 'out'}).toArray();
 
@@ -550,37 +517,6 @@ export class AvlComponent {
         if (secondIndex === 0) return second.getTarget();
 
         return undefined;
-    }
-
-    // private markAsVisited(index: number): void {
-    //     const element: Node = this.graphVisualizerComponent.getNode(index);
-    //
-    //     this.animationSteps.push({
-    //         actions: [
-    //             {element, actionType: OgmaAnimationActionType.ADD_CLASS, actionData: {className: ClassName.SECONDARY}},
-    //         ],
-    //     });
-    // }
-
-    // private addNode(parent: Node, index: number, nodeId: number, edgeId: number, value: number): void {
-    //     const node = {id: nodeId, attributes: {text: value.toString()}, data: {index}};
-    //     const edge = {id: edgeId, source: parent.getId(), target: nodeId, data: {}};
-    //
-    //     this.animationSteps.push({
-    //         actions: [
-    //             {element: parent, actionType: OgmaAnimationActionType.ADD_NODE, actionData: node},
-    //             {element: parent, actionType: OgmaAnimationActionType.ADD_EDGE, actionData: edge},
-    //             {
-    //                 element: parent,
-    //                 actionType: OgmaAnimationActionType.ADD_CLASS_BY_ID,
-    //                 actionData: {id: nodeId, className: ClassName.PATH},
-    //             },
-    //         ],
-    //     });
-    // }
-
-    private nodeRawData(node: Node): any {
-        return {id: node.getId(), attributes: {text: node.getAttribute('text')}, data: {index: +node.getData('index')}};
     }
 
     private edgeRawData(edge: Edge): any {
